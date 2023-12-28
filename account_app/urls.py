50a4217656488fdb0cee92ebe15747a363921e30
ali kazemzadeh <alikazemzadeh16@gmail.com>
2023-12-28 11:12:02 +0330

    add logs for all views and add log out view

--- account_app/urls.py
+++ account_app/urls.py
@@ -6,7 +6,8 @@ from . import views
 
 
 urlpatterns = [
-    path('account/login', TokenObtainPairView.as_view(), name="login"),
+    path('account/login', views.UserLoginView.as_view(), name="login"),
+    path('account/logout', views.LogoutView.as_view(), name="logout"),
     path('account/user_info', views.UserInfoView.as_view(), name="user_info"),
     path('account/edit_username', views.UserNameEditView.as_view(), name="edit_username"),
     path('account/edit_email', views.UserEditEmailView.as_view(), name="edit_email"),
--- account_app/views.py
+++ account_app/views.py
@@ -1,28 +1,88 @@
-from django.shortcuts import render
+from django.contrib.admin.models import LogEntry, CHANGE
+from django.contrib.auth import authenticate
+from django.contrib.contenttypes.models import ContentType
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.views import TokenViewBase
+from rest_framework_simplejwt.serializers import TokenRefreshSerializer
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
 from .serializers import *
+
+
+def user_log(user, message):
+    LogEntry.objects.log_action(
+        user_id=user.pk,
+        content_type_id=ContentType.objects.get_for_model(user).pk,
+        object_id=user.pk,
+        object_repr=str(user),
+        action_flag=CHANGE,
+        change_message=f"{message}"
+    )
+
+
+class UserLoginView(APIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data, partial=True)
+        if serializer.is_valid():
+            username = serializer.validated_data.get('username')
+            password = serializer.validated_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                refresh = RefreshToken.for_user(user)
+                user_log(user, 'کاربر وارد سیستم شد.')
+                return Response({
+                    'refresh': str(refresh),
+                    'access': str(refresh.access_token),
+                })
+            else:
+                return Response({'error': 'نام کاربری یا رمز عبور اشتباه است.'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = LogoutSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            refresh_token = serializer.validated_data.get('refresh')
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            user_log(request.user, 'کاربر از سیستم خارج شد.')
+            return Response({'response': 'کاربر از سیستم خارج شد.'}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 
 class UserInfoView(APIView):
     permission_classes = [IsAuthenticated]
+
     def get(self, request):
         user = request.user
         serializer = UserInfoSerializer(user)
+        user_log(request.user, "کاربر پروفایل خود را مشاهده کرد.")
         return Response(data=serializer.data, status=status.HTTP_200_OK)
+
 
 class UserNameEditView(APIView):
     permission_classes = [IsAuthenticated]
     serializer_class = EditUserNameSerializer
+
     def put(self, request):
         data = request.data
         serializer = self.serializer_class(request.user, data, partial=True)
         if serializer.is_valid():
             serializer.save()
+            user_log(request.user, "کاربر نام کاربری خود را تغییر داد")
             return Response({'message': 'نام کاربری تغییر یافت'}, status=status.HTTP_200_OK)
         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class UserEditEmailView(APIView):
     permission_classes = [IsAuthenticated]
@@ -33,6 +93,7 @@ class UserEditEmailView(APIView):
         serializer = self.serializer_class(request.user, data, partial=True)
         if serializer.is_valid():
             serializer.save()
+            user_log(request.user, "کاربر ایمیل خود را تغییر داد")
             return Response({'message': 'ایمیل با موفقیت تغییر یافت'}, status=status.HTTP_200_OK)
         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -51,12 +112,8 @@ class ChangePasswordView(APIView):
             if user.check_password(password):
                 user.set_password(new_password)
                 user.save()
+                user_log(user, "کاربر رمز عبور  خود را تغییر داد")
                 return Response({'message': 'رمز عبور با موفقیت تغییر یافت.'}, status=status.HTTP_200_OK)
             else:
                 return Response({'message': 'رمز عبور اشتباه است.'}, status=status.HTTP_400_BAD_REQUEST)
         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-
-
-
